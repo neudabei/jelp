a8c82d08dd2d89b5253c955988ffec78b22637af
neudabei <rschultzejena@gmail.com>
2015-10-19 20:14:02 +0100

    flesh out users controller and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Welcome to Jelp, #{@user.full_name}. Thank you for registering!"
+      redirect_to root_path
+    else
+      flash[:error] = "You could not be registered."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :password)
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,47 @@ describe UsersController do
       expect(assigns(:user)).to be_an_instance_of(User)
     end
 
-    it "renders the new template"
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST create" do
+    context "with valid input" do
+      before do
+        post :create, user: Fabricate.attributes_for(:user)
+      end
+
+      it "creates a new user record if submission is valid" do
+        expect(User.count).to eq(1)
+      end
+
+      it "redirects to the root path if submission is valid" do
+        expect(response).to redirect_to root_path
+      end
+
+      it "sets the flash notice if submission is valid" do
+        expect(flash[:notice]).not_to be_empty
+      end
+    end
+
+    context "with invalid input" do
+      before do
+        post :create, user: {full_name: "Max Mustermann", email: "max@domain.com"}
+      end
+
+      it "doesn't create a new record if submission is invalid" do
+        expect(User.count).to eq(0)
+      end
+
+      it "renders the new template if submission is invalid" do
+        expect(response).to render_template :new
+      end
+
+      it "sets the flash error if submission is invalid" do
+        expect(flash[:error]).not_to be_empty
+      end
+    end
   end
 end
