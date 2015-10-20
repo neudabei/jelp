2ac3ff69a8933246dd5e99bb72eac369b31f95c1
neudabei <rschultzejena@gmail.com>
2015-10-20 13:03:02 +0100

    add user id as foreign key to business table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020092637) do
+ActiveRecord::Schema.define(version: 20151020120055) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151020092637) do
     t.string   "website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
