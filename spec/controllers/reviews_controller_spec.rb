7ae0c721cb065da4a0806d2ddee635a018fa9960
neudabei <rschultzejena@gmail.com>
2015-10-20 13:17:18 +0100

    use more readable time syntax for review controller test

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -18,8 +18,8 @@ describe ReviewsController do
       user2 = Fabricate(:user)
       business1 = Fabricate(:business)
       business2 = Fabricate(:business)
-      review1 = Review.create(user: user1, business: business1, body: "Great business!", stars: 4, created_at: (Time.now - (60*60*24)))
-      review2 = Review.create(user: user2, business: business1, body: "Very good service!", stars: 3, created_at: (Time.now - (60*60*12)))
+      review1 = Review.create(user: user1, business: business1, body: "Great business!", stars: 4, created_at: 2.days.ago)
+      review2 = Review.create(user: user2, business: business1, body: "Very good service!", stars: 3, created_at: 1.day.ago)
       review3 = Review.create(user: user2, business: business2, body: "Not that good. Don't recommend it", stars: 0)
       get :index
       expect(assigns(:reviews)).to eq([review3, review2, review1])
