5680097b98f5418186a94bab057061a5ac8110ff
neudabei <rschultzejena@gmail.com>
2015-10-26 14:06:35 +0000

    fix application helper test which failed due to non unique email address

--- spec/controllers/businesses_controller_spec.rb
+++ spec/controllers/businesses_controller_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 describe BusinessesController do
-  describe "GET index" do
-    it "sets the @businesses variable" do
-      business1 = Fabricate(:business)
-      business2 = Fabricate(:business)
-      get :index
-      expect(assigns(:businesses)).to eq([business1, business2])
-    end
-
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-  end
-
   describe "GET new" do
     it "sets the @business variable" do
       get :new
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,7 +4,7 @@ describe "ApplicationHelper" do
   describe "#review_average" do
     let(:store) {Fabricate(:business)}
     let(:john) {Fabricate(:user)}
-    let(:andrea) {Fabricate(:user)}
+    let(:andrea) {Fabricate(:user, email: "andrea@domain.com")}
 
     it "correctly calculates the average of stars from reviews" do
       review1 = Review.create(body: "great business", stars: 4, business_id: store.id, user_id: john.id)
