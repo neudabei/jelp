d35b8c3884be18e9a4883f4d9ded775fd8f0e0f7
neudabei <rschultzejena@gmail.com>
2015-10-27 11:06:16 +0000

    switch to postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151020120055) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "businesses", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
