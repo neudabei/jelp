901421e4ae47bfd29d5eb07702a08fda69cc25d2
neudabei <rschultzejena@gmail.com>
2015-10-30 16:03:29 +0000

    write tests for business-category association in business model

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -4,4 +4,16 @@ describe Business do
   it {should validate_presence_of(:name)}
   it {should belong_to(:user)}
   it {should have_many(:reviews)}
+  it {should have_many(:business_categories)}
+  it {should have_many(:categories).through (:business_categories)}
+
+
+  it "is in many categories through business_categories" do
+    italian_restaurant = Business.create(name: "Italian Restaurant", description: "We serve pasta and pizza and also have a bar", website: "www.italian-restaurant.com" )
+    restaurants = Category.create(name: "restaurants", description: "all the places you can eat")
+    bars = Category.create(name: "bars", description: "a place where alcohol is served")
+    italian_restaurant.categories << restaurants
+    italian_restaurant.categories << bars
+    expect(italian_restaurant.categories).to include(restaurants, bars)
+  end
 end
